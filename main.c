8200e200aa71e538f027f7f0548e33d41690a2ac
JeaCMejia <84648776+JeaCMejia@users.noreply.github.com>
2022-08-11 19:57:16 -0500

    Update main.c
    Comentarios

--- main.c
+++ main.c
@@ -6,22 +6,25 @@
 #include "esp_adc_cal.h"
 #include <unistd.h>
 
-//#if CONFIG_IDF_TARGET_ESP32
+//Configurar el canal (GPIO33) y el ancho de banda (0 a 4095)
 static const adc1_channel_t channel = ADC1_CHANNEL_5;
 static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
-//#endif
 
+//Configurar atenuación - rango de voltajes de lectura (150 a 2450 mV)
 static const adc_atten_t atten = ADC_ATTEN_DB_11;
 
 void app_main(void)
 {
+    //Incializar el lector analogico
     adc1_config_width(width);
     adc1_config_channel_atten(channel, atten);
     uint32_t adc_reading = 0;
    
     while (1) {
+        //Lectura de la señal
         adc_reading = adc1_get_raw(channel);
         printf("%d\n", adc_reading);
+        //Delay para obtener una frecuencia de muestreo de 100Hz
         vTaskDelay(1);
     }
 }
